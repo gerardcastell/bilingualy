cae24d4370d9eca73f0e4059c02078614beeb6ac
Gerard <gerardcastell97@gmail.com>
2020-04-17 22:01:39 +0200

    [Updated] added stepper library

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -7,29 +7,25 @@ import {
   Link,
   ListInput,
   List,
+  Fab,
+  FabButtons,
+  FabButton,
   Icon,
   Button,
 } from "framework7-react";
 import { useSelector, useDispatch } from "react-redux";
 import GridDnd from "../components/core/Grid/GridDnd";
 import { searchPictograms } from "../services/arasaac";
+import Stepper from "../components/core/Stepper";
 import allActions from "../redux/actions";
 import { useFirestoreConnect } from "react-redux-firebase";
 
 import { f7 } from "framework7-react";
 
 const createPage = () => {
-  const [searchResults, setSearchResults] = useState([]);
-  const [word, setWord] = useState("");
-  const stories = useSelector((state) => state.firestore.ordered.prueba);
   const currentStory = useSelector((state) => state.socialStory);
-  const dispatch = useDispatch();
-  useFirestoreConnect({collection: 'prueba', orderBy:['createdAt', 'desc']});
 
-  const handleChange = (event) => {
-    let id = event.target.value;
-    setWord(id);
-  };
+  const dispatch = useDispatch();
 
   const getPictogramList = async () => {
     const text = word;
@@ -39,115 +35,32 @@ const createPage = () => {
     setSearchResults(response);
   };
 
-  const selectPictogram = (id) => {
-    dispatch(allActions.socialStoryActions.addPictogram(id));
-  };
-
-  const saveSocialStory = () => {
-    dispatch(allActions.socialStoryActions.createSocialStory(currentStory));
-  };
-
-  const showCurrentStory = () => {
-    return (
-      <div>
-        {currentStory.map((item, idx) => (
-          <img
-            src={item}
-            key={idx}
-            alt=''
-            style={{ height: "100px", width: "100px" }}
-          />
-        ))}
-      </div>
-    );
-  };
-
-  const downloadSocialStories = () => {
-    console.log(stories);
-    f7.dialog.alert((stories? `${stories.length} lodaded` : 'No story loaded'),'Social stories')
-  };
-
-  const showSearchResults = () => {
-    if (searchResults.length) {
-      return (
-        <div>
-          {searchResults.map((item, idx) => (
-            <img
-              src={item}
-              key={idx}
-              alt=''
-              style={{ height: "100px", width: "100px" }}
-              onClick={() => selectPictogram(item)}
-            />
-          ))}
-        </div>
-      );
-    }
-  };
-
   return (
     <Page>
-      <Navbar title='About' backLink='Back' />
-      <BlockTitle>About My App</BlockTitle>
+      <Navbar title='Create your social story' backLink='Back' />
+      <Fab position='left-bottom' slot='fixed' text='Add'>
+        <Icon ios='f7:plus' aurora='f7:plus' md='material:add'></Icon>
+        <Icon ios='f7:xmark' aurora='f7:xmark' md='material:close'></Icon>
+        <FabButtons position='top'>
+          <FabButton label='Pictogram'>
+            <Icon md='material:portrait' />
+          </FabButton>
+          <FabButton label='Photo'>
+            <Icon md='material:photo_camera' />
+          </FabButton>
+          <FabButton label='Draw'>
+            <Icon md='material:brush' />
+          </FabButton>
+        </FabButtons>
+      </Fab>
       <Block>
-        <h1>My social Story</h1>
-        {/* {showCurrentStory()} */}
-        <GridDnd elements={currentStory} />
+        {/* <GridDnd elements={currentStory} /> */}
+        <Stepper />
+        <Button fill>Search</Button>
       </Block>
-      <Block strong>
-        {/* {currentUser.loggedIn ? (
-          <>
-            <h1>Hello, {currentUser.user.name}</h1>
-            <button onClick={() => dispatch(allActions.userActions.logOut())}>
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <h1>Login</h1>
-            <button
-              onClick={() => dispatch(allActions.userActions.setUser(user))}
-            >
-              Login as Rei
-            </button>
-          </>
-        )}
-        <h1>Counter: {counter}</h1>
-        <button onClick={() => dispatch(allActions.counterActions.increment())}>
-          Increase Counter
-        </button>
-        <button onClick={() => dispatch(allActions.counterActions.decrement())}>
-          Decrease Counter
-        </button> */}
-      </Block>
-      <Block>
-        <List noHairlinesMd>
-          <ListInput
-            label='Pictogram'
-            floatingLabel
-            type='text'
-            placeholder='Write a word'
-            clearButton
-            onChange={handleChange}
-          >
-            <Icon f7='search' slot='media' />
-          </ListInput>
-        </List>
-        <Button fill onClick={getPictogramList}>
-          Search
-        </Button>
-        <Button fill onClick={saveSocialStory} style={{ marginTop: "10px" }}>
-          Save Social Story
-        </Button>
-        <Button
-          fill
-          onClick={downloadSocialStories}
-          style={{ marginTop: "10px" }}
-        >
-          Download social stories
-        </Button>
-        {showSearchResults()}
-      </Block>
+      <Button raised round>
+        Round
+      </Button>
     </Page>
   );
 };
